850a13642a24f1d942a9a86a5d754067ba140654
sandy-icam28 <sandycraft89116@gmail.com>
2026-03-22 18:50:18 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -6,6 +6,6 @@ int main() {
   printf("a + b = %d\n", a+b);
   printf("a - b = %d\n", a-b);
   printf("a * b = %d\n", a*b),
-  printf("a / b = %f\n", a/b);
+  printf("a / b = %d\n", a/b);
   printf("reste de a / b = %d\n", a%b);
 }
