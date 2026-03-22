7620742187fdf499cbadad753e711273fce3b9e4
sandy-icam28 <sandycraft89116@gmail.com>
2026-03-22 18:03:58 +0100

    sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -4,31 +4,25 @@
 int main() {
 printf("Taille des types en C (en octets) :\n\n");
 
-    // Types caractère
-    printf("char               : %zu octets\n", sizeof(char));
-    printf("signed char        : %zu octets\n", sizeof(signed char));
-    printf("unsigned char      : %zu octets\n\n", sizeof(unsigned char));
+    printf("char : %zu octets\n", sizeof(char));
+    printf("signed char : %zu octets\n", sizeof(signed char));
+    printf("unsigned char : %zu octets\n\n", sizeof(unsigned char));
 
-    // Types entiers courts
-    printf("short              : %zu octets\n", sizeof(short));
+    printf("short : %zu octets\n", sizeof(short));
     printf("unsigned short     : %zu octets\n\n", sizeof(unsigned short));
 
-    // Types entiers standards
-    printf("int                : %zu octets\n", sizeof(int));
-    printf("unsigned int       : %zu octets\n\n", sizeof(unsigned int));
+    printf("int : %zu octets\n", sizeof(int));
+    printf("unsigned int : %zu octets\n\n", sizeof(unsigned int));
 
-    // Types entiers longs
-    printf("long               : %zu octets\n", sizeof(long));
-    printf("unsigned long      : %zu octets\n\n", sizeof(unsigned long));
+    printf("long : %zu octets\n", sizeof(long int));
+    printf("unsigned long : %zu octets\n\n", sizeof(unsigned long int));
 
-    // Très grands entiers
-    printf("long long          : %zu octets\n", sizeof(long long));
-    printf("unsigned long long : %zu octets\n\n", sizeof(unsigned long long));
+    printf("long long : %zu octets\n", sizeof(long long int));
+    printf("unsigned long long : %zu octets\n\n", sizeof(unsigned long long int));
 
-    // Types flottants
-    printf("float              : %zu octets\n", sizeof(float));
-    printf("double             : %zu octets\n", sizeof(double));
-    printf("long double        : %zu octets\n", sizeof(long double));
+    printf("float : %zu octets\n", sizeof(float));
+    printf("double : %zu octets\n", sizeof(double));
+    printf("long double : %zu octets\n", sizeof(long double));
 
     return 0;
 }
