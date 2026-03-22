aefcfa11889936655d506014b51fe09ae100b14e
sandy-icam28 <sandycraft89116@gmail.com>
2026-03-22 17:55:04 +0100

    cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -4,7 +4,7 @@
 int main() {
 double pi = 3.1416;
 double r = 10;  
-    printf("L'air du cercle = %f", pi*(r*r));
+    prinwtf("L'air du cercle = %f", pi*(r*r));
    printf("Le périmètre du cercle = %f", pi*(r+r));
 }
 
