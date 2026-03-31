6114afa4455ea7467972e3e6b3afe8c1f7149c90
sandy-icam28 <sandycraft89116@gmail.com>
2026-03-31 22:19:14 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -2,15 +2,19 @@
 
 int main() {
     int compteur = 5;
-    for (int i = 1; i <= compteur; i++) {
-        for (int j = 1; j <= i; j++) {
-            if (j == 1 || j == i || i == compteur) {
-                printf("* ");
-            } else {
-                printf("# ");
+    if (compteur >=10) {
+        printf("Compteur doit etre strictement inferieur à 10");
+    } else {
+        for (int i = 1; i <= compteur; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == compteur) {
+                    printf("* ");
+                } else {
+                    printf("# ");
+                }
             }
+            printf("\n");
         }
-        printf("\n");
     }
-return 0;
+    return 0;
 }
